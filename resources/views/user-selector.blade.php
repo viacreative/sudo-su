eb0b556e779bd1bbeff117f3eb00b64c2f4462f9
Xiaohui Lam <xiaohuilam@users.noreply.github.com>
2020-03-03 12:45:43 +0800

    makes font-awesome be able to be configured

--- resources/views/user-selector.blade.php
+++ resources/views/user-selector.blade.php
@@ -1,4 +1,4 @@
-<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="{{config('sudosu.font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css')}}">
 <link rel="stylesheet" href="/sudo-su/css/app.css">
 
 <div class="sudoSu">
